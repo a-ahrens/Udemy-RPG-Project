105807647e04842828cd8cc2ec7856f6467736d3
Anthony Ahrens <anthony.l.ahrens@gmail.com>
2024-03-29 11:59:20 -0500

    Player Movement - Added ground check and wall check gizmos. Added logic to detect ground layers.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,13 @@ public class Player : MonoBehaviour
     public float moveSpeed = 8f;
     public float jumpForce;
 
+    [Header("Collission Info")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private float wallCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+
     #region Components
     public Animator anim {  get; private set; }
     public Rigidbody2D rb { get; private set; }
@@ -53,5 +60,15 @@ public class Player : MonoBehaviour
     {
         rb.velocity = new Vector2(xVelocity, YVelocity);
     }
+
+    public bool IsGroundDetected() => Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
+    }
+
+
 
 }
